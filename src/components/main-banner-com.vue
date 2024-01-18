901ac7c21f85d3dcb5209762fa79aee80aba6d91
Tharusha Lakshan <34594866+Tharushal@users.noreply.github.com>
2024-01-18 10:14:05 +0530

    Update main-banner-com.vue

--- src/components/main-banner-com.vue
+++ src/components/main-banner-com.vue
@@ -4,7 +4,7 @@
             <div class="navigation">
                 <div class="lg">
                     <div class="logo-box">
-                        <img class="img-logo" src="../assets/images/logo.png" alt="">
+                        <img class="img-logo" src="../assets/images/logow.png" alt="">
                     </div>
                     <a class="a-link" href="/#home">
                         <!-- <font-awesome-icon class="img-link-icon" :icon="['fas', 'home']" /> -->
@@ -50,7 +50,7 @@
                                     class="btn-close btn-close-white"></button>
                             </div>
                             <div class="logo-box">
-                                <img class="img-logo" src="../assets/images/logo.png" alt="">
+                                <img class="img-logo" src="../assets/images/logow.png" alt="">
                             </div>
                             <a class="a-link" @click="mobileNavigationIsVisible = false;" href="/#home">
                                 <p class="txt-link">Home</p>
